5fee0a4db535178974479af99618b295bfc43739
IORTREGA <iortega@nearbpo.com>
2017-02-12 18:26:54 -0600

    Ejemplo router de métodos básicos

--- Ejemplos/Router/ejemplo1.js
+++ Ejemplos/Router/ejemplo1.js
@@ -2,6 +2,7 @@ var express = require('express'),
     request = require('supertest');
 
 describe('Ejemplos Routing' , () => {
+    
     it('Ejemplo Basico' , (done) => {
         var app = express();
         app.get('/', function (req, res) {
@@ -15,5 +16,39 @@ describe('Ejemplos Routing' , () => {
                 resp.should.be.equal('Hola Mundo');
                 done();
             });
+    });
+
+    describe('Ejemplo router de métodos básicos' , () => {
+        var app = express();
+
+        before ( () => {
+            app.get('/', function (req, res) {
+                res.send('GET request to the homepage')
+            });
+
+            app.post('/', function (req, res) {
+                res.send('POST request to the homepage')    
+            })
+        });
+        
+        it('Método GET' , (done) => {
+            request(app)
+                .get('/')
+                .end ( ( err , res) => {
+                    var resp = res.text;
+                    resp.should.be.equal('GET request to the homepage');
+                    done();
+                });
+        });
+
+        it('Método POST' , (done) => {
+            request(app)
+                .post('/')
+                .end ( ( err , res) => {
+                    var resp = res.text;
+                    resp.should.be.equal('POST request to the homepage');
+                    done();
+                });
+        });
     });
 });
